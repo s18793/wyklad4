eb132a783451e4fd5a7eaf10d3650571c6a46d20
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-03-25 13:58:27 +0100

    studentcontroller

---
+++ wyklad4/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace wyklad4.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+
+            return Ok();
+        }
+    }
+}
