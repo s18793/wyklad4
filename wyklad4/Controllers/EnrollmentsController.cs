aee5c5a72ca166b1b1901f2fbe97c3754573e033
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-04-02 11:10:12 +0200

    Request/response (400)

--- wyklad4/Controllers/EnrollmentsController.cs
+++ wyklad4/Controllers/EnrollmentsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using wyklad4.DTOs;
 
 namespace wyklad4.Controllers
 {
@@ -13,8 +14,26 @@ namespace wyklad4.Controllers
     {
 
         [HttpPost]
-        public IActionResult EnrollStudent(EnrollmentsController nStudent) {
+        public IActionResult EnrollStudent(EnrollStudentRequest request) {
             //Dtos - przerzucenie jakis danych miedzy 2 pkt
+
+
+            if (request.FirstName == null || request.LastName == null || request.IndexNumber==null
+               || request.Birthday==null || request.Studies==null)
+            {
+                return BadRequest("Nie zostały podane wszystkie wartości");
+            }
+
+            var st=new Student();
+            st.FirstName = request.FirstName;
+            st.LastName = request.LastName;
+            st.IndexNumber = request.IndexNumber;
+            st.Birthday = request.Birthday;
+            st.Studies = request.Studies;
+
+
+            var response = new EnrollStudentResponse();
+            response.LastName = st.LastName;
 
 
 
---
+++ wyklad4/DTOs/Request/EnrollStudentResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wyklad4.DTOs
+{
+    public class EnrollStudentResponse
+    {
+        public string LastName { get; set; }
+
+        public int Semester
+        {
+            get; set;
+        }
+
+        public DateTime BeginDate { get; set; }
+    }
+}
