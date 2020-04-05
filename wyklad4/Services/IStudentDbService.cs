d3afdd75029d42cdbaa4da278b573df20a8d71cc
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-04-05 12:46:30 +0200

    IDbService

---
+++ wyklad4/Services/IStudentDbService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wyklad4.DTOs;
+using wyklad4.Models;
+
+namespace wyklad4.Services
+{
+    interface IStudentDbService
+    {
+        // void EnrollStudent(EnrollStudentRequest request);
+        //  void PromoteStudents(int semester, string studies);
+
+        Enrollment EnrollStudent(EnrollStudentRequest student);
+        Enrollment PromoteStudents(int semester, string studies);
+    }
+}
---
+++ wyklad4/Services/SqlServerStudentDbService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wyklad4.DTOs;
+
+namespace wyklad4.Services
+{
+    public class SqlServerStudentDbService : IStudentDbService
+    {
+        public EnrollStudentRequest EnrollStudent(EnrollStudentRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PromoteStudents(int semester, string studies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
