a4008024f1cb0a5bb3c238ceabdef57f803f54db
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-04-09 10:49:23 +0200

    swagger wykład

---
+++ wyklad4/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wyklad4.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            //Our code
+            await _next(httpContext);
+        }
+    }
+}
--- wyklad4/Startup.cs
+++ wyklad4/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Bibliography;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace wyklad4
 {
@@ -25,6 +27,13 @@ namespace wyklad4
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+
+            //1
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo { Title = "student App", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +43,20 @@ namespace wyklad4
             {
                 app.UseDeveloperExceptionPage();
             }
+            //api/v1/students domyslny api/studnets
+            //api/v2/students         m
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(config =>
+            {
+                config.SwaggerEndpoint("/swagger/v1/swagger.json", "Students App Api");
+            });
+
+
+
+
+
 
             app.UseRouting();
 
